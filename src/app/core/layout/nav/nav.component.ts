97b72155ab2c4a1d32c296fa3e2f752d3010b4b1
Abanobshafeeq <abanobshafeeq100@gmail.com>
2026-06-09 16:38:16 +0300

    add nav and global style

--- src/app/core/layout/nav/nav.component.ts
+++ src/app/core/layout/nav/nav.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge'; // Great for a cart item count
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule,
+    MatBadgeModule
+  ],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
-
+  // You can connect this to your Cart signal later
+  cartItemCount = 3; 
 }
