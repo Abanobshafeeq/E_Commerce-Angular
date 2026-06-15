f3c6a8b629555a40bc496e220550f33bda54395d
Abanobshafeeq <abanobshafeeq100@gmail.com>
2026-06-15 12:19:08 +0300

    cart service

---
+++ src/app/core/Shared/services/cart.service.ts
@@ -0,0 +1,33 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { Product } from '../../../Features/Products/models/product.model';
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  cartItems = signal<CartItem[]>([]);
+
+  totalItems = computed(() => {
+    return this.cartItems().reduce((total, item) => total + item.quantity, 0);
+  });
+  totalPrice = computed(() => {
+    return this.cartItems().reduce((total, item) => total + item.product.price * item.quantity, 0);
+  });
+  addToCart(product: Product) {
+    this.cartItems.update(items => {
+      const existingItem = items.find(item => item.product.id === product.id);
+      if (existingItem) {
+        return items.map(item =>
+          item.product.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      } else {
+        return [...items, { product, quantity: 1 }];
+      }
+  });  
+}
+}
